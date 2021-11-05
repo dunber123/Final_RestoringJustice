9784ecfab690f7db26cd1c2e752c052d8904db00
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-11-05 16:50:00 -0500

    major update/restructure
    added project commands, too.

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const ClientsRouter = require("./router/client");
+
+// set up our express app
+const app = express();
+
+// Setup static files to serve our built Vue app
+const staticFilesPath = path.resolve(__dirname, "../frontend/dist");
+const staticFiles = express.static(staticFilesPath);
+app.use(staticFiles);
+
+// Global app config
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// Setup mongo connection
+mongoose.connect(process.env.database_url);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+// Setup routes
+app.use("/client", ClientsRouter);
+// Serve our frontend
+app.get("/", (_req, res) => {
+    const p = path.resolve(__dirname, '../frontend/dist/index.html');
+    res.sendFile(p);
+});
+// 404 route
+app.use((_req, res) => {
+  res.status(404).send("<h1>unable to find that</h1>");
+});
+
+// Setup app port
+process.env.port = process.env.port || 3000;
+
+// listen for requests
+app.listen(process.env.port, function () {
+  console.log("Ready to Go! Backend listening on port : " + process.env.port);
+});
