6789427baf0f4d2a4527f9e11e36b01fdaca9b5c
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-11-06 15:50:09 -0500

    work on intake form

--- frontend/src/components/IntakeForm.vue
+++ frontend/src/components/IntakeForm.vue
@@ -1,90 +1,131 @@
 <template>
-  <div>
-    <div class="m-2">
-      <label for="caseNumber">Case Number:</label>
-      <b-form-input
-        id="caseNumber"
-        v-model="caseNumber"
-        placeholder="Enter Case #"
-        trim
-      ></b-form-input>
-    </div>
+  <b-container>
+    <b-row class="justify-content-md-center">
+      <b-col col md="5">
+        <div class="my-2">
+          <label for="caseNumber">Case Number:</label>
+          <b-form-input id="caseNumber" v-model="caseNumber" placeholder="Enter Case #" trim></b-form-input>
+        </div>
+      </b-col>
+      <b-col col md="5">
+        <div class="my-2">
+          <label for="clientNumber">Client Number:</label>
+          <b-form-input id="clientNumber" v-model="clientNumber" placeholder="Enter Client #" trim></b-form-input>
+        </div>
+      </b-col>
+    </b-row>
 
-    <div class="m-2">
-      <label for="clientNumber">Client Number:</label>
-      <b-form-input
-        id="clientNumber"
-        v-model="clientNumber"
-        placeholder="Enter Client #"
-        trim
-      ></b-form-input>
-    </div>
+    <b-row class="justify-content-center">
+      <b-col col md="5">
+        <div class="my-2">
+          <label for="startDatePicker">Start Date:</label>
+          <input type="Date" class="form-control" v-model="startDate" />
+        </div>
+      </b-col>
+      <b-col col md="5">
+        <div class="my-2">
+          <label for="closeDatePicker">Close Date:</label>
+          <input type="Date" class="form-control" v-model="closeDate" />
+        </div>
+      </b-col>
+    </b-row>
 
-    <div class="m-2">
-      <label for="startDatePicker">Start Date:</label>
-      <input type="Date" class="form-control" v-model="startDate">
-    </div>
+    <h3>General Information</h3>
+    <b-row class="justify-content-center my-2">
+      <b-col col md="4">
+        <label for="general.firstName">First Name:</label>
+        <b-form-input id="general.firstName" v-model="generalInformation.firstName" placeholder="First Name:" trim></b-form-input>
+      </b-col>
+      <b-col col md="4">
+        <label for="general.lastName">Last Name:</label>
+        <b-form-input id="general.lastName" v-model="generalInformation.lastName" placeholder="Last Name:" trim></b-form-input>
+      </b-col>
+      <b-col col md="4">
+        <label for="general.middleName">Middle Name:</label>
+        <b-form-input id="general.middleName" v-model="generalInformation.middleName" placeholder="Middle Name:" trim></b-form-input>
+      </b-col>
+    </b-row>
 
-    <div class="m-2">
-      <label for="closeDatePicker">Close Date:</label>
-      <input type="Date" class="form-control" v-model="closeDate">
-    </div>
+    <b-row class="justify-content-center my-2">
+      <b-col col md="6">
+        <label for="general.birthDate">Birth Date:</label>
+        <input id="general.birthDate" type="Date" class="form-control" v-model="generalInformation.birthdate" />
+      </b-col>
+      <b-col col md="6">
+        <label for="general.ssn">SSN:</label>
+        <b-form-input id="general.ssn" v-model="generalInformation.ssn" placeholder="SSN" trim></b-form-input>
+      </b-col>
+    </b-row>
 
-    <div>
-      <h3>General Information</h3>
+    <b-row class="justify-content-center my-2">
+      <b-col col md="6">
+        <label for="general.identificationNumber">ID/TX Drivers License:</label>
+        <b-form-input id="general.identificationNumber" v-model="generalInformation.identificationNumber" placeholder="ID or TX DL" trim></b-form-input>
+      </b-col>
+      <b-col col md="3" style="border: 1px solid gray;">
+        <label for="general.isUsCitizen">Is US Citizen?:</label>
+        <b-form-radio-group id="general.isUsCitizen">
+          <b-form-radio v-model="generalInformation.isUsCitizen" name="general.isUsCitizen.y" value="true">Yes</b-form-radio>
+          <b-form-radio v-model="generalInformation.isUsCitizen" name="general.isUsCitizen.n" value="false">No</b-form-radio>
+        </b-form-radio-group>
+      </b-col>
+      <b-col col md="3" style="border: 1px solid gray;">
+        <label for="general.gender">Gender:</label>
+        <b-form-radio-group id="general.gender">
+          <b-form-radio v-model="generalInformation.gender" name="general.gender.f" value="female">Female</b-form-radio>
+          <b-form-radio v-model="generalInformation.gender" name="general.gender.m" value="male">Male</b-form-radio>
+        </b-form-radio-group>
+      </b-col>
+    </b-row>
 
-    </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
-import 'bootstrap-vue-3'
-  export default {
-    // computed: {
-    //   caseNumberState() {
-    //     return this.name.length > 2 ? true : false
-    //   }
-    // },
-    data() {
-      return {
-        caseNumber: '',
-        clientNumber: '',
-        startDate: null,
-        closeDate: null,
-        generalInformation: {
-          lastName: '',
-          firstName: '',
-          middleName: '',
-          birthdate: '',
-          ssn: '',
-          isUsCitizen: '',
-          gender: '',
-          identificationNumber: ''
-        },
-        residenceInformation: {
-          streetAddress: '',
-          city: '',
-          state: '',
-          zip: '',
-          county: '',
-          totalRent: '',
-          isUtilitiesIncluded: '',
-          isSubsidized: '',
-          isSingleParent: '',
-          heightWeight: '',
-          isInCustody: '',
-          inCustodyLocation: null,
-          isAdelphoi: '',
-          adelphoiName: '',
-        },
-        contactInformation: {
-
-        }
-      }
-    }
-  }
+import "bootstrap-vue-3";
+export default {
+  computed: {
+    isUsCitizenLabel() {
+      if (this.generalInformation.isUsCitizen === null) return "";
+      return this.generalInformation.isUsCitizen ? "Yes" : "No";
+    },
+  },
+  data() {
+    return {
+      caseNumber: "",
+      clientNumber: "",
+      startDate: null,
+      closeDate: null,
+      generalInformation: {
+        lastName: "",
+        firstName: "",
+        middleName: "",
+        birthdate: "",
+        ssn: "",
+        isUsCitizen: null,
+        gender: null,
+        identificationNumber: "",
+      },
+      residenceInformation: {
+        streetAddress: "",
+        city: "",
+        state: "",
+        zip: "",
+        county: "",
+        totalRent: "",
+        isUtilitiesIncluded: "",
+        isSubsidized: "",
+        isSingleParent: "",
+        heightWeight: "",
+        isInCustody: "",
+        inCustodyLocation: null,
+        isAdelphoi: "",
+        adelphoiName: "",
+      },
+      contactInformation: {},
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,5 +9,5 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 const app = createApp(App);
 app.use(BootstrapVue3);
 app.use(router);
-app.config.globalProperties.$hostname = "http://localhost:3000";
+app.config.globalProperties.$hostname = "http://localhost:4000";
 app.mount('#app');
